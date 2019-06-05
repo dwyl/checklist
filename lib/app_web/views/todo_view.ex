0690934b999bdc347fc93fe52b288f82d1ff6b8e
Robert Francis <robertefrancis18@gmail.com>
2019-06-05 12:50:22 +0100

    displays formatted atom to user while allowing form to be submitted #1

--- lib/app_web/views/todo_view.ex
+++ lib/app_web/views/todo_view.ex
@@ -2,11 +2,15 @@ defmodule AppWeb.TodoView do
   use AppWeb, :view
 
   def statuslist do
-    Enum.map(App.StatusList.statuslist, fn (s) ->
-        Atom.to_string(s)
-        |> String.replace("_", " ")
-        |> String.capitalize
-      end)
-    # |> IO.inspect(label: "statuslist string")
+    Enum.map(App.StatusList.statuslist, fn(atom) ->
+      {atom_to_human_str(atom), atom}
+    end)
+  end
+
+  def atom_to_human_str(atom) do
+    atom
+    |> Atom.to_string()
+    |> String.replace("_", " ")
+    |> String.capitalize
   end
 end
--- lib/ecto_enums.ex
+++ lib/ecto_enums.ex
@@ -1,11 +1,2 @@
 import EctoEnum
-defenum StatusEnum,
-  # unassigned: 0,
-  # assigned: 1,
-  # in_progress: 2,
-  # awaiting_review: 3,
-  # in_review: 4,
-  # dependent: 5,
-  # blocked: 6,
-  # complete: 7
-Enum.with_index(App.StatusList.statuslist)
+defenum StatusEnum, Enum.with_index(App.StatusList.statuslist)
