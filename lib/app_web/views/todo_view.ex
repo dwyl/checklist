c69ff8ef3e67ae2caa79342cfa712841468fef89
nelsonic <nelson+github@dwyl.io>
2019-06-05 12:23:58 +0100

    stringify statuslist for displaying in view #1

--- lib/app_web/views/todo_view.ex
+++ lib/app_web/views/todo_view.ex
@@ -1,3 +1,12 @@
 defmodule AppWeb.TodoView do
   use AppWeb, :view
+
+  def statuslist do
+    Enum.map(App.StatusList.statuslist, fn (s) ->
+        Atom.to_string(s)
+        |> String.replace("_", " ")
+        |> String.capitalize
+      end)
+    # |> IO.inspect(label: "statuslist string")
+  end
 end
--- lib/ecto_enums.ex
+++ lib/ecto_enums.ex
@@ -1,10 +1,11 @@
 import EctoEnum
 defenum StatusEnum,
-  unassigned: 0,
-  assigned: 1,
-  in_progress: 2,
-  awaiting_review: 3,
-  in_review: 4,
-  dependent: 5,
-  blocked: 6,
-  complete: 7
+  # unassigned: 0,
+  # assigned: 1,
+  # in_progress: 2,
+  # awaiting_review: 3,
+  # in_review: 4,
+  # dependent: 5,
+  # blocked: 6,
+  # complete: 7
+Enum.with_index(App.StatusList.statuslist)
