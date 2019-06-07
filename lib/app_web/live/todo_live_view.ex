4ca35fc709c9b317793c8f4091d0b2d084a341ad
nelsonic <nelson+github@dwyl.io>
2019-06-07 14:15:10 +0100

    add comments to handle_event "update_status" for #2 https://git.io/fjzEm

--- lib/app_web/live/todo_live_view.ex
+++ lib/app_web/live/todo_live_view.ex
@@ -26,9 +26,12 @@ defmodule AppWeb.TodoLive do
   def handle_event("update_status", value, socket) do
     IO.inspect(value, label: "value")
 
-
-    status = App.StatusList.statuslist() |> Enum.random()
-    App.Ctx.get_todo!(value) |> App.Ctx.update_todo(%{status: status})
+    # value should be todo.id_status so we can split on "_" to get the id and status ...
+    # @robstallion this is the part that is not yet working ... https://git.io/fjzEm
+    parts = String.split(value, "_")
+    id = parts[0]
+    status = parts[1]
+    App.Ctx.get_todo!(id) |> App.Ctx.update_todo(%{status: status})
 
     todos = App.Ctx.list_todos_by_priority()
     AppWeb.Endpoint.broadcast_from(self(), @topic, "update", %{todos: todos})
