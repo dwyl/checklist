9e147ef3c220ec043e78c7e5b861424b3f913b68
nelsonic <nelson+github@dwyl.io>
2019-06-07 13:49:18 +0100

    [WiP] attempting to add status <select> on index page for #2

--- lib/app_web/live/todo_live_view.ex
+++ lib/app_web/live/todo_live_view.ex
@@ -7,7 +7,7 @@ defmodule AppWeb.TodoLive do
     AppWeb.TodoView.render("index_live.html", assigns)
   end
 
-  def mount(session, socket) do
+  def mount(_session, socket) do
     AppWeb.Endpoint.subscribe(@topic)
     todos = App.Ctx.list_todos_by_priority()
     {:ok, assign(socket, todos: todos)}
@@ -15,6 +15,18 @@ defmodule AppWeb.TodoLive do
 
   def handle_event("change_status", value, socket) do
     IO.inspect(value, label: "value")
+    status = App.StatusList.statuslist() |> Enum.random()
+    App.Ctx.get_todo!(value) |> App.Ctx.update_todo(%{status: status})
+
+    todos = App.Ctx.list_todos_by_priority()
+    AppWeb.Endpoint.broadcast_from(self(), @topic, "update", %{todos: todos})
+    {:noreply, assign(socket, todos: todos)}
+  end
+
+  def handle_event("update_status", value, socket) do
+    IO.inspect(value, label: "value")
+
+
     status = App.StatusList.statuslist() |> Enum.random()
     App.Ctx.get_todo!(value) |> App.Ctx.update_todo(%{status: status})
 
--- lib/app_web/views/todo_view.ex
+++ lib/app_web/views/todo_view.ex
@@ -7,10 +7,35 @@ defmodule AppWeb.TodoView do
     end)
   end
 
+  def statusliststring do
+    Enum.map(App.StatusList.statuslist, fn(atom) ->
+
+      atom
+      |> Atom.to_string()
+    end)
+  end
+
   def atom_to_human_str(atom) do
     atom
     |> Atom.to_string()
+    |> human_readble_status()
+  end
+
+  def human_readble_status(str) do
+    str
     |> String.replace("_", " ")
     |> String.capitalize
   end
+
+  def selected(todo_status, status) do
+    # IO.inspect(todo_status, label: "todo_status")
+    # IO.inspect(status, label: "status")
+
+    if Atom.to_string(todo_status) == status do
+      "selected"
+    else
+      ""
+    end
+  end
+
 end
